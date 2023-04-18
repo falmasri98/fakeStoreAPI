1e1f5a70dd4768ff2dd9c798181d9651b9c7cc95
Farah <farahalmasri562@gmail.com>
2023-04-18 11:37:53 +0300

    edited

--- app.js
+++ app.js
@@ -1,38 +1,55 @@
 class Product {
-    constructor(title, price, description, image) {
-      this.title = title;
-      this.price = price;
-      this.description = description;
-      this.image = image;
-    }
+  constructor(title, price, description, image) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.image = image;
   }
-  
-  const productList = [];
-  
-  fetch("https://fakestoreapi.com/products")
-    .then((response) => response.json())
-    .then((data) => {
-      for (let i = 0; i < 20; i++) {
-        const product = new Product(
-          data[i].title,
-          data[i].price,
-          data[i].description,
-          data[i].image
-        );
-        productList.push(product);
-      }
-  
-      const main = document.querySelector("main");
-      main.innerHTML = productList
-        .map(
-          (product) =>
-            `<div class="product-card">
-              <img src="${product.image}" alt="${product.title}" />
-              <h2>${product.title}</h2>
-              <p>${product.description}</p>
-              <h3>$${product.price.toFixed(2)}</h3>
-            </div>`
-        )
-        .join("");
+}
+
+const productList = [];
+
+fetch("https://fakestoreapi.com/products")
+  .then((response) => response.json())
+  .then((data) => {
+    data.slice(0, 20).forEach((item) => {
+      const product = new Product(
+        item.title,
+        item.price,
+        item.description,
+        item.image
+      );
+      productList.push(product);
     });
-  
+
+    const main = document.querySelector("main");
+
+    const productCards = productList.map((product) => {
+      const productCard = document.createElement("div");
+      productCard.classList.add("product-card");
+    
+      const productImage = document.createElement("img");
+      productImage.src = product.image;
+      productImage.alt = product.title;
+    
+      const productTitle = document.createElement("h2");
+      productTitle.textContent = product.title;
+    
+      const productDescription = document.createElement("p");
+      productDescription.textContent = product.description;
+    
+      const productPrice = document.createElement("h3");
+      productPrice.textContent = `$${product.price.toFixed(2)}`;
+    
+      productCard.appendChild(productImage);
+      productCard.appendChild(productTitle);
+      productCard.appendChild(productDescription);
+      productCard.appendChild(productPrice);
+    
+      return productCard;
+    });
+    
+    main.append(...productCards);
+    
+    });
+
